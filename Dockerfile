26d99f1eeafeebe6805675fab03e7bf33a0783e5
Sergey Siryk <uamang@gmail.com>
2017-10-28 23:47:00 +0300

    Refactoring scripts, moving to rsync

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM scalified/alpine-cron:latest
 
-ENV RCLONE_HOME /usr/bin
+ENV RCLONE_HOME /opt/rclone
 ENV RCLONE_CONFIG_DIR /root/.config/rclone
 ENV BACKUP_SCRIPTS_DIR /root/.scripts
 ENV CRONTABS_DIR /var/spool/cron/crontabs
@@ -9,36 +9,54 @@ ENV CRON_LOG_DIR /var/log/crond
 ENV SSH_DIR /root/.ssh
 ENV BACKUP_DIR /root/.backup
 ENV RCLONE_URL https://downloads.rclone.org/rclone-current-linux-amd64.zip
+ENV RCLONE_ARCHIVE rclone.zip
 
-RUN apk add --update --no-cache curl
+RUN apk add --update --no-cache curl \
+    unzip
 
-RUN curl -O $RCLONE_URL \
-    && mkdir rclone \
-    && unzip rclone-current-linux-amd64.zip -d rclone
+RUN mkdir -p $RCLONE_HOME
 
-RUN cd rclone/rclone-*-linux-amd64  \
-    && mkdir -p $RCLONE_HOME \
-    && cp rclone $RCLONE_HOME \    
-    && chown root:root $RCLONE_HOME/rclone \
+RUN curl $RCLONE_URL --output $RCLONE_HOME/$RCLONE_ARCHIVE \
+    && cd $RCLONE_HOME \ 
+    && unzip -j $RCLONE_ARCHIVE \
+    && rm $RCLONE_ARCHIVE
+    
+RUN chown root:root $RCLONE_HOME/rclone \
     && chmod 755 $RCLONE_HOME/rclone
 
-RUN mkdir -p $RCLONE_CONFIG_DIR $BACKUP_SCRIPTS_DIR $CRONTABS_DIR \	
-	$CRON_LOG_DIR $SSH_DIR $BACKUP_DIR
+RUN ln -s $RCLONE_HOME/rclone /usr/bin/rclone
+    
+RUN mkdir -p $RCLONE_CONFIG_DIR \
+    $BACKUP_SCRIPTS_DIR \
+    $CRONTABS_DIR \ 
+    $CRON_LOG_DIR \
+    $SSH_DIR \
+    $BACKUP_DIR
 
-COPY scripts/backup_dir.sh $BACKUP_SCRIPTS_DIR
+COPY scripts/backup_dir.sh \
+     scripts/archiveRemotePath.sh \
+     scripts/arhiveLocalPath.sh \
+     scripts/backup_mongo.sh \
+     scripts/downloadFiles.sh \
+     scripts/downloadPath.sh \
+     scripts/rcloneUploadPath.sh $BACKUP_SCRIPTS_DIR/
+
+RUN dos2unix $BACKUP_SCRIPTS_DIR/backup_dir.sh \
+    $BACKUP_SCRIPTS_DIR/archiveRemotePath.sh \
+    $BACKUP_SCRIPTS_DIR/arhiveLocalPath.sh \
+    $BACKUP_SCRIPTS_DIR/backup_mongo.sh \
+    $BACKUP_SCRIPTS_DIR/downloadFiles.sh \
+    $BACKUP_SCRIPTS_DIR/downloadPath.sh \
+    $BACKUP_SCRIPTS_DIR/rcloneUploadPath.sh          
 
 RUN chmod u+x $BACKUP_SCRIPTS_DIR/backup_dir.sh \
     && echo "TZ=UTC" >> $ROOT_CRONTABS_FILE \
-	&& echo "00      20      *       *       1-5     $BACKUP_SCRIPTS_DIR/backup_dir.sh -h swupp-squash-tm.mircloud.us -s /opt/backup/squash-tm -t /tmp/backup-squash-tm -a true >> $CRON_LOG_DIR/backup.log" >> $ROOT_CRONTABS_FILE \
-	&& echo "00      21      *       *       1-5     $BACKUP_SCRIPTS_DIR/backup_dir.sh -h swupp-squash-tm.mircloud.us -s /opt/backup/squash-tm -t /tmp/backup-squash-tm >> $CRON_LOG_DIR/backup.log" >> $ROOT_CRONTABS_FILE \
-	&& echo "00      08      *       *       1-5     $BACKUP_SCRIPTS_DIR/backup_dir.sh -h swupp-squash-tm.mircloud.us -s /opt/backup/squash-tm -t /tmp/backup-squash-tm -m \"*.txt\" >> $CRON_LOG_DIR/backup.log" >> $ROOT_CRONTABS_FILE \
-	&& echo "00      09      *       *       1-5     $BACKUP_SCRIPTS_DIR/backup_dir.sh -h swupp-squash-tm.mircloud.us -s /opt/backup/squash-tm -t /tmp/backup-squash-tm -f 1.txt >> $CRON_LOG_DIR/backup.log" >> $ROOT_CRONTABS_FILE
-
-RUN dos2unix $BACKUP_SCRIPTS_DIR/backup_dir.sh
+    && echo "00      20      *       *       1-5     $BACKUP_SCRIPTS_DIR/backup_dir.sh -h swupp-squash-tm.mircloud.us -s /opt/backup/squash-tm -t /tmp/backup-squash-tm -a true >> $CRON_LOG_DIR/backup.log" >> $ROOT_CRONTABS_FILE \
+    && echo "00      21      *       *       1-5     $BACKUP_SCRIPTS_DIR/backup_dir.sh -h swupp-squash-tm.mircloud.us -s /opt/backup/squash-tm -t /tmp/backup-squash-tm >> $CRON_LOG_DIR/backup.log" >> $ROOT_CRONTABS_FILE \
+    && echo "00      08      *       *       1-5     $BACKUP_SCRIPTS_DIR/backup_dir.sh -h swupp-squash-tm.mircloud.us -s /opt/backup/squash-tm -t /tmp/backup-squash-tm -m .txt >> $CRON_LOG_DIR/backup.log" >> $ROOT_CRONTABS_FILE \
+    && echo "00      09      *       *       1-5     $BACKUP_SCRIPTS_DIR/backup_dir.sh -h swupp-squash-tm.mircloud.us -s /opt/backup/squash-tm -t /tmp/backup-squash-tm -m 1.txt >> $CRON_LOG_DIR/backup.log" >> $ROOT_CRONTABS_FILE
           
-VOLUME $RCLONE_HOME
-VOLUME $RCLONE_CONFIG_DIR
-VOLUME $BACKUP_SCRIPTS_DIR
-VOLUME $CRONTABS_DIR
-VOLUME $SSH_DIR
 VOLUME $BACKUP_DIR
+VOLUME $SSH_DIR
+VOLUME $RCLONE_CONFIG_DIR
+VOLUME $CRONTABS_DIR
--- scripts/backup_dir.sh
+++ scripts/backup_dir.sh
@@ -5,15 +5,13 @@ BACKUP_DIR=~/.backup
 RCLONE_REMOTE_NAME="auto-backup"
 
 # Parsing arguments
-while getopts "a:s:u:f:m:h:t:q" opt
+while getopts "s:h:t:m:a:q" opt
 do
     case $opt in
-      s) BACKUP_DIR_PATH=$OPTARG;;
-      f) BACKUP_FILE=$OPTARG;;
-      m) BACKUP_FILE_MASK=$OPTARG;;                                                     
-      u) UPLOAD_PATH=$OPTARG;;
+      s) SOURCE_INPUT_PATH=$OPTARG;;
       h) SOURCE_HOST=$OPTARG;;
-      t) TEMP_LOCAL_BACKUP_PATH=$OPTARG;;
+      t) DESTINATION_OUTPUT_PATH=$OPTARG;;  
+      m) BACKUP_FILE_MASK=$OPTARG;;                                                                 
       a) ARCHIVE_REMOTELY=$OPTARG;;  
       q) QUITE=1;;
       :|\?) exit 1;;
@@ -22,181 +20,61 @@ done
 
 # Required params
 [ -z $SOURCE_HOST ] && echo "-h <SOURCE_HOST> is required" && exit 1
-[ -z $BACKUP_DIR_PATH ] && echo "-s <BACKUP_DIR_PATH> is required" && exit 1
-[ -z $TEMP_LOCAL_BACKUP_PATH ] && echo "-t <TEMP_LOCAL_BACKUP_PATH> is required" && exit 1
+[ -z $SOURCE_INPUT_PATH ] && echo "-s <SOURCE_INPUT_PATH> is required" && exit 1
+[ -z $DESTINATION_OUTPUT_PATH ] && echo "-t <DESTINATION_OUTPUT_PATH> is required" && exit 1
 
 backup() {
-        if [ -n "$BACKUP_DIR_PATH" ] && [ -n "$ARCHIVE_REMOTELY" ]
+        if [ -n "$ARCHIVE_REMOTELY" ]
         then
             if [ $ARCHIVE_REMOTELY = true ]
             then
                 BACKUP_ARCHIVE_NAME="$(date '+%Y-%m-%d-%H%M').tar.gz"
                 echo "BACKUP_ARCHIVE_NAME = ${BACKUP_ARCHIVE_NAME}"
 
-                FULL_REMOTE_ARCHIVE_SAVED_PATH="/tmp"
-                ssh ${SOURCE_HOST} "cd ${BACKUP_DIR_PATH} && tar -cvzf ${FULL_REMOTE_ARCHIVE_SAVED_PATH}/${BACKUP_ARCHIVE_NAME} ${BACKUP_DIR_PATH}" 2> /dev/null
-
-                if [ $? == 0 ]; then
-                    echo "[INFO][$(date -u)]: Successfully was created the archive ${BACKUP_ARCHIVE_NAME} on remote host ${SOURCE_HOST}"
-                    echo "Start coping the backup archive ${BACKUP_ARCHIVE_NAME} from remote host ${SOURCE_HOST}"
-
-                    UPLOAD_DIR=${BACKUP_DIR}/${SOURCE_HOST}
-                    
-                    mkdir -p ${BACKUP_DIR}
-                    mkdir -p ${UPLOAD_DIR}
-                    
-                    scp ${USER}@${SOURCE_HOST}:${FULL_REMOTE_ARCHIVE_SAVED_PATH}/${BACKUP_ARCHIVE_NAME} ${UPLOAD_DIR}
-
-                    if [ $? == 0 ]; then
-                        echo "[INFO][$(date -u)]: Successfully was copied the archive ${BACKUP_ARCHIVE_NAME} from remote host ${SOURCE_HOST}"
-                        echo "[INFO][$(date -u)]: Start uploading on GDrive ..."
-
-                        upload
-                        exit 0;                
-                    else
-                        echo "[INFO][$(date -u)]: Failed to copy the file ${BACKUP_FILE}."
-                        exit 1;
-                    fi
-
-                else
-                    echo "[INFO][$(date -u)]: Failed to create the archive on remote host ${SOURCE_HOST}"
-                    exit 1;
-                fi
+                DESTINATION_REMOTE_PATH="/tmp"
+                
+                sh archiveRemotePath.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -m ${DESTINATION_REMOTE_PATH}/${BACKUP_ARCHIVE_NAME}
+                
+                sh downloadFiles.sh -u ${USER} -s ${SOURCE_HOST} -f ${DESTINATION_REMOTE_PATH} -m ${BACKUP_ARCHIVE_NAME} -d ${BACKUP_DIR}/${SOURCE_HOST}                
+                
+                sh rcloneUploadPath.sh -s ${BACKUP_DIR}/${SOURCE_HOST} -n ${RCLONE_REMOTE_NAME} -d ${SOURCE_HOST}                                                 
             else
                 echo "The optional parameter -a must be set to \"true\""
                 exit 1;
             fi    
             
             exit 0;
-        elif [ -n "$BACKUP_DIR_PATH" ] && [ -n "$BACKUP_FILE" ]
+        elif [ -n "${BACKUP_FILE_MASK}" ]        
         then
-            mkdir -p ${TEMP_LOCAL_BACKUP_PATH}
-            scp ${USER}@${SOURCE_HOST}:${BACKUP_DIR_PATH}/${BACKUP_FILE} ${TEMP_LOCAL_BACKUP_PATH}
-            echo "BACKUP SOURCE_HOST = ${SOURCE_HOST}"
-            echo "BACKUP_DIR_PATH = ${BACKUP_DIR_PATH}"
-            echo "BACKUP_FILE = ${BACKUP_FILE}"
+            sh downloadFiles.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -d ${DESTINATION_OUTPUT_PATH} -m ${BACKUP_FILE_MASK}
 
-            if [ $? == 0 ]; then
-                echo "[INFO][$(date -u)]: Successfully the file ${BACKUP_FILE} was copied."                
-            else
-                echo "[INFO][$(date -u)]: Failed to copy the file ${BACKUP_FILE}."
-                exit 1;
-            fi
+            sh arhiveLocalPath.sh -s ${DESTINATION_OUTPUT_PATH} -d ${BACKUP_DIR}/${SOURCE_HOST}
             
-            archive
-            upload
+            rm -rf ${DESTINATION_OUTPUT_PATH}
+            
+            sh rcloneUploadPath.sh -s ${BACKUP_DIR}/${SOURCE_HOST} -n ${RCLONE_REMOTE_NAME} -d ${SOURCE_HOST}                    
             
             exit 0;
-        elif [ -n "${BACKUP_DIR_PATH}" ] && [ -n "${BACKUP_FILE_MASK}" ]        
-        then    
-            MASK=$(echo "${BACKUP_FILE_MASK}" | sed 's/.*\*//g')            
-
-            echo "-------------------------------------------------"
-            echo "MASK = ${MASK}"
-            echo "BACKUP_FILE_MASK = ${BACKUP_FILE_MASK}"
-            echo "-------------------------------------------------"
-
-            FILES=$(ssh ${USER}@${SOURCE_HOST} ls ${BACKUP_DIR_PATH} |grep ${MASK})
-            echo "-------------------------------------------------"
-            echo "REMOTE FILES for COPING = ${FILES}"
-            echo "-------------------------------------------------"    
-            
-            for file in ${FILES}                                                                               
-            do                                                                                                                        
-                echo "Start coping the ${file} file ..."
-                mkdir -p ${TEMP_LOCAL_BACKUP_PATH}
-                scp ${USER}@${SOURCE_HOST}:${BACKUP_DIR_PATH}/${file} ${TEMP_LOCAL_BACKUP_PATH}
-            done
-
-            if [ $? == 0 ]; then
-                echo "[INFO][$(date -u)]: Successfully all files were copied."                
-            else
-                echo "[INFO][$(date -u)]: Failed to copy the files."
-                exit 1;
-            fi
-
-            archive
-            upload              
-            
-            exit 0;
-        elif [ -n "$BACKUP_DIR_PATH" ]
+        elif [ -n "SOURCE_INPUT_PATH" ]
         then
-            mkdir -p ${TEMP_LOCAL_BACKUP_PATH}
+            sh downloadPath.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -d ${DESTINATION_OUTPUT_PATH}
 
-            echo "Runs a recursive copy of the entire directory on the remote machine ${SOURCE_HOST} ..."
+            sh arhiveLocalPath.sh -s ${DESTINATION_OUTPUT_PATH} -d ${BACKUP_DIR}/${SOURCE_HOST}
 
-            scp -r ${USER}@${SOURCE_HOST}:${BACKUP_DIR_PATH}/* ${TEMP_LOCAL_BACKUP_PATH}
-
-            if [ $? == 0 ]; then
-                echo "[INFO][$(date -u)]: Successfully the copy of the directory was done."                
-            else
-                echo "[INFO][$(date -u)]: Failed to copy the directory."
-                exit 1;
-            fi
-
-            archive
-            upload
+            rm -rf ${DESTINATION_OUTPUT_PATH}
+            
+            sh rcloneUploadPath.sh -s ${BACKUP_DIR}/${SOURCE_HOST} -n ${RCLONE_REMOTE_NAME} -d ${SOURCE_HOST}                     
 
             exit 0;
         else
             help
-            exit 1;
         fi      
-}
-
-archive() {
-    echo "[INFO][$(date -u)]: Start archiving ..."
-
-    if [ -n "${TEMP_LOCAL_BACKUP_PATH}" ] && [ -d "${TEMP_LOCAL_BACKUP_PATH}" ]
-        then
-            BACKUP_ARCHIVE_NAME=`date "+%Y-%m-%d-%H%M"`.tar.gz
-            UPLOAD_DIR=${BACKUP_DIR}/${SOURCE_HOST}
-            FULL_BACKUP_FILE_PATH=${UPLOAD_DIR}/${BACKUP_ARCHIVE_NAME}          
-
-            mkdir -p ${BACKUP_DIR}
-            mkdir -p ${UPLOAD_DIR}
-            
-            tar -cvzf ${FULL_BACKUP_FILE_PATH} ${TEMP_LOCAL_BACKUP_PATH} 2> /dev/null               
-            
-            if [ $? == 0 ]; then
-                echo "[INFO][$(date -u)]: Successfully was created the backup archive."
-                
-                rm -rf ${TEMP_LOCAL_BACKUP_PATH}
-
-                if [ $? == 0 ]; then
-                    echo "[INFO][$(date -u)]: Successfully was deleted the directory of temp backup."
-                else
-                    echo "[INFO][$(date -u)]: Failed to delete the directory of temp backup."
-                    exit 1;
-                fi
-            else
-                echo "[INFO][$(date -u)]: Failed to create a tar archive."
-                exit 1;
-            fi
-        else
-            echo "[INFO][$(date -u)]: Archiving error. Path to the archive directory = ${TEMP_LOCAL_BACKUP_PATH}"
-            exit 1;        
-        fi
-}
-
-upload() {
-     echo "[INFO][$(date -u)]: Start uploading the files ..."
-
-     GDRIVE_DIR=${SOURCE_HOST}
-
-     rclone copy ${UPLOAD_DIR} ${RCLONE_REMOTE_NAME}:/${GDRIVE_DIR}
-
-     if [ $? == 0 ]; then
-        echo "[INFO][$(date -u)]: Successfully all files were uploaded to the GDRIVE folder /${GDRIVE_DIR}"
-        exit 0;
-     else
-        echo "[INFO][$(date -u)]: Failed to upload the files to the GDRIVE folder /${GDRIVE_DIR}"
-        exit 1;
-     fi
 }
 
 help() {
     echo "HELP MANUAL of SCRIPT"
+
+    exit 0;
 }
 
 echo "-------------------------------------------------------------------------------------"
