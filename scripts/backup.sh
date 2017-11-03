00a0e43c36bcfaeafb87a6157b9bc54ec637e06a
Sergey Siryk (Office) <serg.siryk@gmail.com>
2017-11-03 18:21:40 +0200

    Backup scripts refactoring due to set -e

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,67 +6,68 @@ USER=root
 BACKUP_DIR=~/.backup
 RCLONE_REMOTE_NAME="scalified-remote"
 
-validate_extended() {
-    validate_base
-    
-    # Required params    
-    [ -z $DESTINATION_OUTPUT_PATH ] && echo "-o|--output-path <DESTINATION_OUTPUT_PATH> is required" && exit 1    
-}
+validate() {
+    validate_arg() {
+        local arg="${!1}"
+        local message="${2}"
+        if [[ -z ${arg} ]]; then
+            echo "${message}"
+            exit 1
+        fi
+    }
 
-validate_base() {
-    # Required params
-    [ -z $SOURCE_HOST ] && echo "-s|--source-host <SOURCE_HOST> is required" && exit 1
-    [ -z $SOURCE_INPUT_PATH ] && echo "-i|--input-path <SOURCE_INPUT_PATH> is required" && exit 1
-    [ -z $REMOTE_PATH ] && echo "-r|--remote-path <REMOTE_PATH> is required" && exit 1
+    validate_arg 'SOURCE_HOST' '-s|--source-host <SOURCE_HOST> is required'
+    validate_arg 'SOURCE_INPUT_PATH' '-i|--input-path <SOURCE_INPUT_PATH> is required'
+    validate_arg 'REMOTE_PATH' '-r|--remote-path <REMOTE_PATH> is required'
+    validate_arg 'DESTINATION_OUTPUT_PATH' '-o|--output-path <DESTINATION_OUTPUT_PATH> is required'
 }
 
 pipe_source_archive() {
-    validate_extended
+    validate
 
-    sh ${BACKUP_SCRIPTS_DIR}/pipe_source_archive.sh -u ${USER} -s ${SOURCE_HOST} -i ${SOURCE_INPUT_PATH} -o ${DESTINATION_OUTPUT_PATH}
-
-    sh ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${DESTINATION_OUTPUT_PATH} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}$REMOTE_PATH
+    ${BACKUP_SCRIPTS_DIR}/pipe_source_archive.sh -u ${USER} -s ${SOURCE_HOST} -i ${SOURCE_INPUT_PATH} -o ${DESTINATION_OUTPUT_PATH}
+    ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${DESTINATION_OUTPUT_PATH} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}${REMOTE_PATH}
 }
 
 keep_source_archive() {
-    validate_extended
+    validate
 
-    BACKUP_ARCHIVE_NAME="$(date '+%Y-%m-%d-%H%M').tar.gz"
-    echo "BACKUP_ARCHIVE_NAME = ${BACKUP_ARCHIVE_NAME}"
+    local backup_archive_name="$(date '+%Y-%m-%d-%H%M').tar.gz"
+    echo "Set ${backup_archive_name} backup archive name"
 
-    DESTINATION_REMOTE_PATH="/tmp"
-                
-    sh ${BACKUP_SCRIPTS_DIR}/archive_remote_path.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -m ${DESTINATION_REMOTE_PATH}/${BACKUP_ARCHIVE_NAME}
-                
-    sh ${BACKUP_SCRIPTS_DIR}/download_files.sh -u ${USER} -s ${SOURCE_HOST} -f ${DESTINATION_REMOTE_PATH} -m ${BACKUP_ARCHIVE_NAME} -d ${BACKUP_DIR}/${SOURCE_HOST}
-                
-    sh ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${BACKUP_DIR}/${SOURCE_HOST} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}$REMOTE_PATH           
+    local destination_remote_path="/tmp"
+
+    ${BACKUP_SCRIPTS_DIR}/archive_remote_path.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -m ${destination_remote_path}/${backup_archive_name}
+    ${BACKUP_SCRIPTS_DIR}/download_files.sh -u ${USER} -s ${SOURCE_HOST} -f ${destination_remote_path} -m ${backup_archive_name} -d ${BACKUP_DIR}/${SOURCE_HOST}
+    ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${BACKUP_DIR}/${SOURCE_HOST} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}${REMOTE_PATH}
 }
 
 backup_files() {
-    validate_extended
+    validate
 
-    sh ${BACKUP_SCRIPTS_DIR}/download_files.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -d ${DESTINATION_OUTPUT_PATH} -m ${BACKUP_FILE_MASK}
+    ${BACKUP_SCRIPTS_DIR}/download_files.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -d ${DESTINATION_OUTPUT_PATH} -m ${BACKUP_FILE_MASK}
+    ${BACKUP_SCRIPTS_DIR}/arhive_local_path.sh -s ${DESTINATION_OUTPUT_PATH} -d ${BACKUP_DIR}/${SOURCE_HOST}
 
-    sh ${BACKUP_SCRIPTS_DIR}/arhive_local_path.sh -s ${DESTINATION_OUTPUT_PATH} -d ${BACKUP_DIR}/${SOURCE_HOST}
-            
     rm -rf ${DESTINATION_OUTPUT_PATH}
-            
-    sh ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${BACKUP_DIR}/${SOURCE_HOST} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}$REMOTE_PATH                    
+
+    ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${BACKUP_DIR}/${SOURCE_HOST} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}${REMOTE_PATH}
 }
 
 backup_path_no_archived() {
-    validate_extended
-
-    local upload_dir_path=${DESTINATION_OUTPUT_PATH}
+    validate
 
     mkdir -p ${DESTINATION_OUTPUT_PATH}
 
-    sh ${BACKUP_SCRIPTS_DIR}/download_path.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -d ${upload_dir_path}
-
-    sh ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${upload_dir_path} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}$REMOTE_PATH
+    ${BACKUP_SCRIPTS_DIR}/download_path.sh -u ${USER} -s ${SOURCE_HOST} -f ${SOURCE_INPUT_PATH} -d ${DESTINATION_OUTPUT_PATH}
+    ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${DESTINATION_OUTPUT_PATH} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}${REMOTE_PATH}
 
     rm -rf ${DESTINATION_OUTPUT_PATH}
+}
+
+display_help() {
+    echo "HELP MANUAL of SCRIPT"
+
+    exit 0;
 }
 
 parse_arguments() {
@@ -74,7 +75,7 @@ parse_arguments() {
     for i in "${!ARGS[@]}"; do
         case "${ARGS[i]}" in
             -h|--help)
-                help
+                display_help
                 ;;
             -s|--source-host)
                 SOURCE_HOST=${ARGS[i+1]}
@@ -95,7 +96,7 @@ parse_arguments() {
             -r|--remote-path)
                 REMOTE_PATH=${ARGS[i+1]}
                 echo "REMOTE_PATH=${REMOTE_PATH}"
-                ;;            
+                ;;
             --keep-source-archive)
                 keep_source_archive
                 exit 0
@@ -108,18 +109,12 @@ parse_arguments() {
                 pipe_source_archive
                 exit 0;
                 ;;
-            --backup-path-no-archived)
+			--backup-path-no-archived)
                 backup_path_no_archived
                 exit 0;
-                ;;            
+                ;;
         esac
     done
-}
-
-help() {
-    echo "HELP MANUAL of SCRIPT"
-
-    exit 0;
 }
 
 echo "-------------------[INFO][$(date -u)]: Running Backup Script -------------------"
--- scripts/backup_mongo.sh
+++ scripts/backup_mongo.sh
@@ -20,59 +20,48 @@ RCLONE_REMOTE_NAME="scalified-remote"
   done
 
 validate() {
-  # Required params
-  if [ -z "${SOURCE_HOST}" ]
-  then
-    echo "-s <SOURCE_HOST> is required" && exit 1
-  fi
+    validate_arg() {
+        local arg="${!1}"
+        local message="${2}"
+        if [[ -z ${arg} ]]; then
+            echo "${message}"
+            exit 1
+        fi
+    }
 
-  if [ -z "${DATABASE_NAME}" ]
-  then
-    echo "-d <DATABASE_NAME> is required" && exit 1
-  fi
-
-  if [ -z "${DESTINATION_OUTPUT_PATH}" ]
-  then
-    echo "-o <DESTINATION_OUTPUT_PATH> is required" && exit 1
-  fi
-
-  if [ -z "${REMOTE_PATH}" ]
-  then
-    echo "-r <REMOTE_PATH> is required" && exit 1
-  fi  
+    validate_arg 'SOURCE_HOST' '-s|--source-host <SOURCE_HOST> is required'
+    validate_arg 'DATABASE_NAME' '-d <DATABASE_NAME> is required'
+    validate_arg 'DESTINATION_OUTPUT_PATH' '-o <DESTINATION_OUTPUT_PATH> is required'
+    validate_arg 'REMOTE_PATH' '-r <REMOTE_PATH> is required'
 }
 
+dump_db() {
+    local archive_name="${1}"
+    echo "About to dump ${DATABASE_NAME} database into ${archive_name}"
+    ssh ${USER}@${SOURCE_HOST} "mkdir -p /tmp"
+    ssh ${USER}@${SOURCE_HOST} "mongodump --archive=/tmp/${archive_name} --gzip --db ${DATABASE_NAME}"
+}
+
+delete_db_dump() {
+    local archive_name="${1}"
+    echo "About to delete ${archive_name} database dump file"
+    ssh ${USER}@${SOURCE_HOST} "rm /tmp/${archive_name}"
+}
 
 backup() {
   echo "Started ${DATABASE_NAME} Mongo DataBase backup at ${SOURCE_HOST} host"
 
   local archive_name=mongo_dump_${DATABASE_NAME}_"$(date '+%Y-%m-%d-%H%M').gz"
-
-  dump_db() {
-    ssh ${USER}@${SOURCE_HOST} "mkdir -p /tmp"
-
-    ssh ${USER}@${SOURCE_HOST} "mongodump --archive=/tmp/${archive_name} --gzip --db ${DATABASE_NAME}"
-  }
-  
-  dump_db
-  
-  echo "[INFO][$(date -u)]: the backup archive ${archive_name} of the Mongo database ${DATABASE_NAME} has been successfully created at ${SOURCE_HOST} host."
+  dump_db "${archive_name}"
+  echo "[INFO][$(date -u)]: Backup archive ${archive_name} of the Mongo database ${DATABASE_NAME} has been successfully created at ${SOURCE_HOST} host."
      	
-  MONGO_BACKUP_DIR=${DESTINATION_OUTPUT_PATH}
-     
-  mkdir -p ${MONGO_BACKUP_DIR}
+  mkdir -p ${DESTINATION_OUTPUT_PATH}
+  sh ${BACKUP_SCRIPTS_DIR}/download_files.sh -u ${USER} -s ${SOURCE_HOST} -f /tmp -d ${DESTINATION_OUTPUT_PATH} -m ${archive_name}
+  sh ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${DESTINATION_OUTPUT_PATH} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}${REMOTE_PATH}
 
-  sh ${BACKUP_SCRIPTS_DIR}/download_files.sh -u ${USER} -s ${SOURCE_HOST} -f /tmp -d ${MONGO_BACKUP_DIR} -m ${archive_name}
-        
-  sh ${BACKUP_SCRIPTS_DIR}/rclone_upload_path.sh -s ${MONGO_BACKUP_DIR} -n ${RCLONE_REMOTE_NAME} -r ${SOURCE_HOST}${REMOTE_PATH}
+  delete_db_dump "${archive_name}"
 
-  delete_dump_db() {
-    ssh ${USER}@${SOURCE_HOST} "rm /tmp/${archive_name}"
-  }
-
-  delete_dump_db
-
-  rm -rf ${MONGO_BACKUP_DIR}
+  rm -rf ${DESTINATION_OUTPUT_PATH}
 }
 
 echo "-------------------[INFO][$(date -u)]: Runing Mongo Backup Script -------------------"
